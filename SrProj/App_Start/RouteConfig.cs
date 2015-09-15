df9a64b54f4e1536af09c0a395e31655b52b506a
Krishna Glick <krishna.glick@ascentis.com>
2015-09-15 14:05:37 -0400

    First!

---
+++ SrProj/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SrProj
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.IgnoreRoute("{file}.js");
+            routes.IgnoreRoute("{file}.html");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SrProj/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SrProj.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
