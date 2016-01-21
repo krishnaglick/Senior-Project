afdd24a014b7a153cdcedebe6457b268ab630100
Krishna Glick <unstoppable411@gmail.com>
2016-01-20 22:59:52 -0500

    I'm uncertain how I want to handle reporting on the backend.

---
+++ SrProj/API/ReportingController.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SrProj.API
+{
+    [AuthorizableController]
+    public class ReportingController : ApiController
+    {
+        public HttpResponseMessage FilterData()
+        {
+            //I dunno what I want to do with this yet.
+            return null;
+        }
+    }
+}
---
+++ Utility/Attribute/FilterAttribute.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+namespace Utility.Attribute
+{
+    public enum FilterableAttributeType
+    {
+        Dropdown = 0,
+
+        Text = 1,
+
+        Number = 2,
+
+        Boolean = 3,
+
+        Date = 4
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public abstract class FilterableAttribute : System.Attribute
+    {
+        public FilterableAttributeType type { get; set; }
+    }
+
+    public class FilterableDropdown : FilterableAttribute
+    {
+        public FilterableDropdown()
+        {
+            this.type = FilterableAttributeType.Dropdown;
+        }
+    }
+
+    public class FilterableText : FilterableAttribute
+    {
+        public FilterableText()
+        {
+            this.type = FilterableAttributeType.Text;
+        }
+    }
+
+    public class FilterableNumber : FilterableAttribute
+    {
+        public FilterableNumber()
+        {
+            this.type = FilterableAttributeType.Number;
+        }
+    }
+
+    public class FilterableBoolean : FilterableAttribute
+    {
+        public FilterableBoolean()
+        {
+            this.type = FilterableAttributeType.Boolean;
+        }
+    }
+
+    public class FilterableDate : FilterableAttribute
+    {
+        public FilterableDate()
+        {
+            this.type = FilterableAttributeType.Date;
+        }
+    }
+}
