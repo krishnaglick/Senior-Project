f400bdf7e5e5d99ed82d4cd667eff95eb936b2e7
eugenesadsarin <esadsarin@gmail.com>
2016-03-01 15:43:31 -0500

    Adding view model.

---
+++ SrProj/Static/viewModels/patronCheckInViewModel.js
@@ -0,0 +1,28 @@
+﻿function CheckInViewModel() {
+    this.controller = 'patronCheckIn';
+    
+    this.validate = function() {
+        var errors= [];
+        if (this. patronCheckIn === '1'){
+            //Patron Check In Validation
+        }
+        return errors;
+    }.bind(this);
+
+    //Patron Check In
+    this.serviceStatus = ko.observable();
+    this.firstName = ko.observable();
+    this.middleName = ko.observable();
+    this.lastName = ko.observable();
+    this.dateOfBirth = ko.obervable();
+    this.gender = ko.observable();
+    this.ethnicity = ko.observable();
+    this.streetAdress = ko.observable();
+    this.apartmentNumber = ko.observable();
+    this.postalCode = ko.observable();
+    this.householdOccupants = ko.observable();
+    this.veteranStatus = ko.observable(false);
+    this.maritalStatus = ko.observable();
+    this.serviceEligibility = ko.observable();
+    this.neccessaryPaperwork = ko.observable(false);
+}.bind(this);
